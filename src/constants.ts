ac9e429bf0685d403a47dc902045ed7ab8d56642
Padunk <abraham.agel@gmail.com>
2020-06-30 14:47:01 +0700

    adding viewport breakpoints

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,21 @@
 export const COLORS = {
-  black: '#222',
-  white: '#f1f1f1',
-}
+  black: "#222",
+  white: "#f1f1f1",
+  gray: "#9e9e9e",
+  grayDark: "#757575",
+  grayLight: "#E0E0E0",
+  primary: "#ffb200",
+  primaryAlpha: "hsla(42, 100%, 50%, 0.8)",
+  secondary: "##1298B3",
+  secondaryLight: "#0DD7FF",
+  secondaryLightAlpha: "rgba(13, 215, 255, 0.8)",
+  tertiary: "#3612B3",
+  tertiaryLight: "#430DFF",
+};
+
+export const BREAKPOINTS = {
+  sm: "(min-width: " + 640 + "px)",
+  md: "(min-width: " + 768 + "px)",
+  lg: "(min-width: " + 1024 + "px)",
+  xl: "(min-width: " + 2056 + "px)",
+};
