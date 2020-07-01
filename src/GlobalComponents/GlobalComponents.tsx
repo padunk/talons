4aef52ef6772f897e15e880e5c040da0a7775f35
Padunk <abraham.agel@gmail.com>
2020-07-01 15:35:08 +0700

    adding Tablet styled and label comp.

--- src/GlobalComponents/GlobalComponents.tsx
+++ src/GlobalComponents/GlobalComponents.tsx
@@ -1,12 +1,13 @@
+import * as React from 'react';
 import styled from "styled-components";
 import { BREAKPOINTS } from "../constants";
 
 export const Container = styled.div<{
-  height: string;
-  background: string;
-  heightXXL: string;
+  height?: string;
+  background?: string;
+  heightXXL?: string;
 }>`
-  width: 100vw;
+  min-width: 100%;
   height: ${(props) => props.height};
   background: ${(props) => props.background};
 
@@ -23,8 +24,22 @@ export const DesktopOnly = styled.span`
   }
 `;
 
+export const TabletAndUp = styled.span`
+  display: none;
+
+  @media ${BREAKPOINTS.md} {
+    display: block;
+  }
+`;
+
 export const MobileOnly = styled.span`
   @media ${BREAKPOINTS.lg} {
     display: none;
   }
 `;
+
+export const UnstyledLabel = (props: any) => (
+  <label {...props}>{props.children}</label>
+);
+
+export const UnstyledImg = (props: any) => <img {...props} alt={props.alt} />;
