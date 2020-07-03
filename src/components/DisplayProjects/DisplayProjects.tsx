68cd49ee264291b42110bec586e9310a28e15d20
Padunk <abraham.agel@gmail.com>
2020-07-03 09:38:04 +0700

    refactor + feat(sort & filter button)

--- src/components/DisplayProjects/DisplayProjects.tsx
+++ src/components/DisplayProjects/DisplayProjects.tsx
@@ -1,160 +1,83 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, UnstyledImg } from "../GlobalComponents/GlobalComponents";
+import {
+  Container,
+  UnstyledImg,
+  UnstyledButton,
+} from "../GlobalComponents/GlobalComponents";
 import styled from "styled-components";
 import { CategoriesList } from "../Categories/CategoriesList";
-import USER_DATA from "../../mockData/USER_DATA.json";
-import PROJECTS_DATA from "../../mockData/PROJECTS_DATA.json";
 import { Spacer } from "../Spacer/Spacer";
-import { BREAKPOINTS, FONT_SIZES, FONTS, COLORS } from "../../constants";
-import { SvgComments } from "../SVG/Comments";
-import { SvgHeart } from "../SVG/Heart";
-import { SvgFrame } from "../SVG/Frame";
+import { ProjectCard } from "../ProjectCard/ProjectCard";
+import { GrFilter, GrSort } from "react-icons/gr";
+import { IconContext } from "react-icons";
+import { COLORS } from "../../constants";
 
 export function DisplayProjects() {
   return (
     <Container>
       <CategoriesList />
       <Spacer size='3rem' />
-      <GridContainerOuter>
-        <div>
-          <GridContainerInner>
-            {USER_DATA.map((data, idx) => (
-              <CardWrapper key={data.id}>
-                <div className='card-wrapper--inner'>
-                  <div className='picture' style={{position: 'relative'}}>
-                    <picture>
-                      <source
-                        srcSet={`https://picsum.photos/1200/900.webp?random=${idx}`}
-                        media='(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)'
-                      />
-                      <source
-                        srcSet={`https://picsum.photos/800/600.webp?random=${idx}`}
-                        media='(min-width: 1440px)'
-                      />
-                      <ProjectImg
-                        src={`https://picsum.photos/600/450.webp?random=${idx}`}
-                        alt='project'
-                        loading='lazy'
-                        width='600'
-                      />
-                    </picture>
-                    <StyledFrame />
-                  </div>
-                  <CardFooter>
-                    <UserData>
-                      <AvatarWrapper>
-                        <img
-                          src={data.avatar}
-                          alt={data.first_name}
-                          width='30'
-                          height='30'
-                          loading='lazy'
-                        />
-                      </AvatarWrapper>
-                      <div className='user-name'>
-                        <UserName>
-                          {data.first_name} {data.last_name}
-                        </UserName>
-                      </div>
-                    </UserData>
-                    <ProjectsData>
-                      <IconsWrapper>
-                        <SvgComments
-                          width='1.4rem'
-                          fill={COLORS.grayTextLight}
-                        />
-                        <ProjectCount>
-                          {PROJECTS_DATA[idx].comments_count}
-                        </ProjectCount>
-                      </IconsWrapper>
-                      <IconsWrapper>
-                        <SvgHeart width='1.4rem' fill={COLORS.grayTextLight} />
-                        <ProjectCount>{PROJECTS_DATA[idx].likes}</ProjectCount>
-                      </IconsWrapper>
-                    </ProjectsData>
-                  </CardFooter>
-                </div>
-              </CardWrapper>
-            ))}
-          </GridContainerInner>
-        </div>
-      </GridContainerOuter>
+      {/* <ProjectCard /> */}
+      <Spacer size='5rem' />
+      <div className='control-wrapper'>
+        <SortFilterWrapper>
+          <SortFilterInner>
+            <SortFilterButton>
+              <GrSort
+                size='1.8rem'
+                style={{ verticalAlign: "middle" }}
+                stroke={COLORS.grayTextMed}
+              />
+            </SortFilterButton>
+          </SortFilterInner>
+          <VerticalLineDivider />
+          <SortFilterInner>
+            <SortFilterButton>
+              <GrFilter
+                size='1.8rem'
+                style={{ verticalAlign: "middle" }}
+                stroke={COLORS.grayTextMed}
+              />
+            </SortFilterButton>
+          </SortFilterInner>
+        </SortFilterWrapper>
+      </div>
     </Container>
   );
 }
 
-const StyledFrame = styled(SvgFrame)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  fill: white;
-`
-
-const ProjectCount = styled.span`
-  padding-left: 5px;
+const VerticalLineDivider = styled.span`
+  display: inline-block;
+  height: 4.2rem;
+  width: 1px;
+  background: ${COLORS.grayTextMed};
 `;
 
-const IconsWrapper = styled.div`
-  flex: 1 1;
-
+const SortFilterButton = styled(UnstyledButton)`
+  text-align: center;
+  outline: none;
+  border: none;
   display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  font-size: ${FONT_SIZES.mdMin};
-`;
+  justify-content: center;
+  padding: 1.2rem 1.4rem;
+  background: ${COLORS.grayLight};
 
-const ProjectsData = styled.div`
-  flex: 1 1;
-  display: flex;
-  color: ${COLORS.grayTextMed};
-`;
-
-const UserName = styled.h4`
-  font-family: ${FONTS.serif};
-  font-size: ${FONT_SIZES.mdMin};
-`;
-
-const AvatarWrapper = styled.div`
-  border-radius: 50%;
-  width: 3rem;
-  height: 3rem;
-  margin-right: 2rem;
-  box-shadow: inset 0 0 0 2px black;
-`;
-
-const UserData = styled.div`
-  display: flex;
-  align-items: center;
-  flex: 2 1;
-`;
-
-const CardFooter = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 1rem;
-`;
-
-const ProjectImg = styled(UnstyledImg)`
-  width: 100%;
-  object-fit: cover;
-`;
-
-const CardWrapper = styled.li`
-  overflow: hidden;
-`;
-
-const GridContainerInner = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(27rem, 1fr));
-  grid-auto-flow: dense;
-  column-gap: 2rem;
-  row-gap: 3rem;
-  padding: 0 1rem;
-
-  @media ${BREAKPOINTS.xs} {
-    padding: 0 2rem;
+  &:hover {
+    background: ${COLORS.whiteAlpha};
   }
 `;
 
-const GridContainerOuter = styled.div``;
+const SortFilterInner = styled.div`
+  flex: 1 1;
+`;
+
+const SortFilterWrapper = styled.div`
+  display: flex;
+  border: 1px solid ${COLORS.grayTextMed};
+  overflow: hidden;
+  position: fixed;
+  bottom: 2rem;
+  left: 2rem;
+  border-radius: 2rem;
+`;
---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,150 @@
+import * as React from "react";
+import styled from 'styled-components';
+import USER_DATA from "../../mockData/USER_DATA.json";
+import PROJECTS_DATA from "../../mockData/PROJECTS_DATA.json";
+import { BREAKPOINTS, FONT_SIZES, FONTS, COLORS } from "../../constants";
+import { SvgComments } from "../SVG/Comments";
+import { SvgHeart } from "../SVG/Heart";
+import { SvgFrame } from "../SVG/Frame";
+import { Container, UnstyledImg } from "../GlobalComponents/GlobalComponents";
+
+export function ProjectCard() {
+  return (
+    <GridContainerOuter>
+      <div>
+        <GridContainerInner>
+          {USER_DATA.map((data, idx) => (
+            <CardWrapper key={data.id}>
+              <div className='card-wrapper--inner'>
+                <div className='picture' style={{ position: "relative" }}>
+                  <picture>
+                    <source
+                      srcSet={`https://picsum.photos/1200/900.webp?random=${idx}`}
+                      media='(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)'
+                    />
+                    <source
+                      srcSet={`https://picsum.photos/800/600.webp?random=${idx}`}
+                      media='(min-width: 1440px)'
+                    />
+                    <ProjectImg
+                      src={`https://picsum.photos/600/450.webp?random=${idx}`}
+                      alt='project'
+                      loading='lazy'
+                      width='600'
+                    />
+                  </picture>
+                  <StyledFrame />
+                </div>
+                <CardFooter>
+                  <UserData>
+                    <AvatarWrapper>
+                      <img
+                        src={data.avatar}
+                        alt={data.first_name}
+                        width='30'
+                        height='30'
+                        loading='lazy'
+                      />
+                    </AvatarWrapper>
+                    <div className='user-name'>
+                      <UserName>
+                        {data.first_name} {data.last_name}
+                      </UserName>
+                    </div>
+                  </UserData>
+                  <ProjectsData>
+                    <IconsWrapper>
+                      <SvgComments width='1.4rem' fill={COLORS.grayTextLight} />
+                      <ProjectCount>
+                        {PROJECTS_DATA[idx].comments_count}
+                      </ProjectCount>
+                    </IconsWrapper>
+                    <IconsWrapper>
+                      <SvgHeart width='1.4rem' fill={COLORS.grayTextLight} />
+                      <ProjectCount>{PROJECTS_DATA[idx].likes}</ProjectCount>
+                    </IconsWrapper>
+                  </ProjectsData>
+                </CardFooter>
+              </div>
+            </CardWrapper>
+          ))}
+        </GridContainerInner>
+      </div>
+    </GridContainerOuter>
+  );
+}
+
+const StyledFrame = styled(SvgFrame)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  fill: white;
+`;
+
+const ProjectCount = styled.span`
+  padding-left: 5px;
+`;
+
+const IconsWrapper = styled.div`
+  flex: 1 1;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: ${FONT_SIZES.mdMin};
+`;
+
+const ProjectsData = styled.div`
+  flex: 1 1;
+  display: flex;
+  color: ${COLORS.grayTextMed};
+`;
+
+const UserName = styled.h4`
+  font-family: ${FONTS.serif};
+  font-size: ${FONT_SIZES.mdMin};
+`;
+
+const AvatarWrapper = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  margin-right: 2rem;
+  box-shadow: inset 0 0 0 2px black;
+`;
+
+const UserData = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 2 1;
+`;
+
+const CardFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+`;
+
+const ProjectImg = styled(UnstyledImg)`
+  width: 100%;
+  object-fit: cover;
+`;
+
+const CardWrapper = styled.li`
+  overflow: hidden;
+`;
+
+const GridContainerInner = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(27rem, 1fr));
+  grid-auto-flow: dense;
+  column-gap: 2rem;
+  row-gap: 3rem;
+  padding: 0 1rem;
+
+  @media ${BREAKPOINTS.xs} {
+    padding: 0 2rem;
+  }
+`;
+
+const GridContainerOuter = styled.div``;
