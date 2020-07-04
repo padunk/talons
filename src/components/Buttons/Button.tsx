19ddc70c97e039c2ff222e8f0f1f01a17eda6fcc
Padunk <abraham.agel@gmail.com>
2020-07-04 09:59:04 +0700

    Button components

---
+++ src/components/Buttons/Button.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import styled from "styled-components";
+import { UnstyledButton } from "../GlobalComponents/GlobalComponents";
+import { COLORS } from "../../constants";
+import { GrFilter, GrSort } from "react-icons/gr";
+
+export function Button(props: any) {
+  if (props.type === "primary") {
+    return <ButtonPrimary {...props}>{props.children}</ButtonPrimary>;
+  }
+  return <ButtonSecondary {...props}>{props.children}</ButtonSecondary>;
+}
+
+export function FilterButton(
+  props: React.ButtonHTMLAttributes<HTMLButtonElement>
+) {
+  return (
+    <Button type='secondary' {...props}>
+      <GrFilter
+        size='1.8rem'
+        style={{ verticalAlign: "middle" }}
+        stroke={COLORS.grayTextMed}
+      />
+    </Button>
+  );
+}
+
+export function SortButton(
+  props: React.ButtonHTMLAttributes<HTMLButtonElement>
+) {
+  return (
+    <Button type='secondary' {...props}>
+      <GrSort
+        size='1.8rem'
+        style={{ verticalAlign: "middle" }}
+        stroke={COLORS.grayTextMed}
+      />
+    </Button>
+  );
+}
+
+const ButtonSecondary = styled(UnstyledButton)`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  padding: 1.2rem 1.4rem;
+  background: ${COLORS.white};
+
+  &:hover {
+    background: ${COLORS.whiteAlpha};
+  }
+`;
+
+const ButtonPrimary = styled(UnstyledButton)`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  padding: 1.2rem 1.4rem;
+  background: ${COLORS.primary};
+
+  &:hover {
+    background: ${COLORS.primaryAlpha};
+  }
+`;
