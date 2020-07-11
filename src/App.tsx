3600df24a50755109602e82eed0900d2a83d87f3
Padunk <abraham.agel@gmail.com>
2020-07-11 17:37:18 +0700

    firebase context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { Switch, Route } from "react-router-dom";
 import { Home } from "./components/Home/Home";
 import { LoginPage } from "./components/LoginPage/LoginPage";
@@ -6,11 +6,25 @@ import { RegisterPage } from "./components/RegisterPage/RegisterPage";
 import { Layout } from "./components/Layout/Layout";
 import { MAIN_ROUTES } from "./constants/constants";
 import { ResetPasswordPage } from "./components/ResetPasswordPage/ResetPasswordPage";
+import { useFirebaseContext } from "./components/Firebase";
 
 function App() {
+  const [authUser, setAuthUser] = React.useState<firebase.User>();
+  const firebaseContext = useFirebaseContext();
+
+  React.useEffect(() => {
+    const listener = firebaseContext.auth.onAuthStateChanged(
+      (authUsr: firebase.User | null) => {
+        authUsr ? setAuthUser(authUsr) : setAuthUser(undefined);
+      }
+    );
+
+    return () => listener();
+  });
+
   return (
     <div>
-      <Layout>
+      <Layout authUser={authUser}>
         <Switch>
           <Route exact path={MAIN_ROUTES.landing}>
             <Home />
