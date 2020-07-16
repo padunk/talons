aed8be1dde012ca1a1d41ee3423ea441ee24e0e5
Padunk <abraham.agel@gmail.com>
2020-07-16 17:31:31 +0700

    adding more routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { Home } from "./components/Home/Home";
 import { LoginPage } from "./components/LoginPage/LoginPage";
 import { RegisterPage } from "./components/RegisterPage/RegisterPage";
 import { Layout } from "./components/Layout/Layout";
-import { MAIN_ROUTES } from "./constants/constants";
+import { MAIN_ROUTES, NAV_ROUTES } from "./constants/constants";
 import { ResetPasswordPage } from "./components/ResetPasswordPage/ResetPasswordPage";
 import { useFirebaseContext } from "./components/Firebase";
+import { Developers } from "./components/Developers/Developers";
+import { UserAccountPage } from "./components/UserAccountPage/UserAccountPage";
 
 function App() {
   const [authUser, setAuthUser] = React.useState<firebase.User>();
@@ -39,6 +41,24 @@ function App() {
             <LoginPage />
           </Route>
           <Route path={MAIN_ROUTES["password-forget"]}>
+            <ResetPasswordPage />
+          </Route>
+          <Route path={MAIN_ROUTES.account}>
+            <UserAccountPage authUser={authUser} />
+          </Route>
+          <Route path={NAV_ROUTES[0].path}>
+            <Developers />
+          </Route>
+          <Route path={NAV_ROUTES[1].path}>
+            <ResetPasswordPage />
+          </Route>
+          <Route path={NAV_ROUTES[2].path}>
+            <ResetPasswordPage />
+          </Route>
+          <Route path={NAV_ROUTES[3].path}>
+            <ResetPasswordPage />
+          </Route>
+          <Route path={NAV_ROUTES[4].path}>
             <ResetPasswordPage />
           </Route>
         </Switch>
