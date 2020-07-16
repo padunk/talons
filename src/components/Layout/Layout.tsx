430794d8219e415f4c9e630da501f3ab586e6cb6
Padunk <abraham.agel@gmail.com>
2020-07-16 17:33:19 +0700

    adding footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,13 @@
-import * as React from 'react'
-import { Navigation } from '../Navigation/Navigation'
+import * as React from "react";
+import { Navigation } from "../Navigation/Navigation";
+import { Footer } from "../Footer/Footer";
 
 export function Layout(props: any) {
   return (
     <React.Fragment>
       <Navigation authUser={props.authUser} />
       {props.children}
+      <Footer />
     </React.Fragment>
-  )
+  );
 }
