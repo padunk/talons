e5b506fb41a6650e212e9a9b06eaa99ccb9dfdf8
Padunk <abraham.agel@gmail.com>
2020-07-25 10:59:01 +0700

    refactor routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import * as React from "react";
-import { Switch, Route } from "react-router-dom";
-import { Home } from "./components/Home/Home";
-import { LoginPage } from "./components/LoginPage/LoginPage";
-import { RegisterPage } from "./components/RegisterPage/RegisterPage";
 import { Layout } from "./components/Layout/Layout";
-import { MAIN_ROUTES, NAV_ROUTES } from "./constants/constants";
-import { ResetPasswordPage } from "./components/ResetPasswordPage/ResetPasswordPage";
 import { useFirebaseContext } from "./components/Firebase";
-import { Developers } from "./components/Developers/Developers";
-import { UserAccountPage } from "./components/UserAccountPage/UserAccountPage";
+import { Routes } from "./routes/Routes";
 
 function App() {
   const [authUser, setAuthUser] = React.useState<firebase.User>();
@@ -27,41 +20,7 @@ function App() {
   return (
     <div>
       <Layout authUser={authUser}>
-        <Switch>
-          <Route exact path={MAIN_ROUTES.landing}>
-            <Home />
-          </Route>
-          <Route exact path={MAIN_ROUTES.home}>
-            <Home />
-          </Route>
-          <Route path={MAIN_ROUTES.register}>
-            <RegisterPage />
-          </Route>
-          <Route path={MAIN_ROUTES.login}>
-            <LoginPage />
-          </Route>
-          <Route path={MAIN_ROUTES["password-forget"]}>
-            <ResetPasswordPage />
-          </Route>
-          <Route path={MAIN_ROUTES.account}>
-            <UserAccountPage authUser={authUser} />
-          </Route>
-          <Route path={NAV_ROUTES[0].path}>
-            <Developers />
-          </Route>
-          <Route path={NAV_ROUTES[1].path}>
-            <ResetPasswordPage />
-          </Route>
-          <Route path={NAV_ROUTES[2].path}>
-            <ResetPasswordPage />
-          </Route>
-          <Route path={NAV_ROUTES[3].path}>
-            <ResetPasswordPage />
-          </Route>
-          <Route path={NAV_ROUTES[4].path}>
-            <ResetPasswordPage />
-          </Route>
-        </Switch>
+        <Routes authUser={authUser} />
       </Layout>
     </div>
   );
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { Home } from "../components/Home/Home";
+import { LoginPage } from "../components/LoginPage/LoginPage";
+import { RegisterPage } from "../components/RegisterPage/RegisterPage";
+import { MAIN_ROUTES, NAV_ROUTES } from "../constants/constants";
+import { ResetPasswordPage } from "../components/ResetPasswordPage/ResetPasswordPage";
+import { Developers } from "../components/Developers/Developers";
+import { UserAccountPage } from "../components/UserAccountPage/UserAccountPage";
+import { Meetups } from "../components/Meetups/Meetups";
+import { Blogs } from "../components/Blogs/Blogs";
+import { Hiring } from "../components/Hiring/Hiring";
+
+export function Routes(props: any) {
+  return (
+    <Switch>
+      <Route exact path={MAIN_ROUTES.landing}>
+        <Home />
+      </Route>
+      <Route exact path={MAIN_ROUTES.home}>
+        <Home />
+      </Route>
+      <Route path={MAIN_ROUTES.register}>
+        <RegisterPage />
+      </Route>
+      <Route path={MAIN_ROUTES.login}>
+        <LoginPage />
+      </Route>
+      <Route path={MAIN_ROUTES["password-forget"]}>
+        <ResetPasswordPage />
+      </Route>
+      <Route path={MAIN_ROUTES.account}>
+        <UserAccountPage authUser={props.authUser} />
+      </Route>
+      <Route path={NAV_ROUTES[0].path}>
+        <Developers />
+      </Route>
+      <Route path={NAV_ROUTES[1].path}>
+        <Home />
+      </Route>
+      <Route path={NAV_ROUTES[2].path}>
+        <Meetups />
+      </Route>
+      <Route path={NAV_ROUTES[3].path}>
+        <Blogs />
+      </Route>
+      <Route path={NAV_ROUTES[4].path}>
+        <Hiring />
+      </Route>
+    </Switch>
+  );
+}
